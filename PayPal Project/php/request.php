d82939541c4cec42c3a16f697d3bf91acbb2e64a
Alan Abraham <47579004+Alantrivandrum@users.noreply.github.com>
2021-07-27 14:48:46 +0100

    Add files via upload

---
+++ PayPal Project/php/request.php
@@ -0,0 +1,16 @@
+<?php
+include 'create_payment.php';
+if ($response) // If form was submited...
+{
+    $api_endpoint = $response["links"][0]["href"]; // Get it into a variable
+    //print_r( "Api Enpoint: " . $api_endpoint); // Print it!
+    //print_r('<br>');
+    //print_r("Request Body: " . $request_body);
+    // print_r();
+
+}
+else{
+    //echo "failure";
+}
+
+?>
