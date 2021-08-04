948d1ee1947c317f35deec788de93acd7b129601
TomMeehan123 <58112746+TomMeehan123@users.noreply.github.com>
2021-08-04 13:43:17 +0100

    Add files via upload

---
+++ PayPal Project/php/create_token.php
@@ -0,0 +1,39 @@
+<?php
+
+$data = $_POST['request_body'];
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $_POST['api_endpoint'],
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_SSL_VERIFYHOST => false,
+  CURLOPT_SSL_VERIFYPEER => false,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_HEADER => false,
+  CURLOPT_HTTPHEADER => array(
+    "Content-Type: application/json",
+    "prefer:return=representation",
+    "Authorization: Bearer " . $_SESSION['access_token']
+  ),
+  CURLOPT_POSTFIELDS => $data)
+);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err)
+{
+  echo $err;
+}
+else
+{
+  $response = json_decode($response);
+
+  $token_id = $response->token_id;
+  $_SESSION['token_id'] = $token_id;
+}        
+
+?>
