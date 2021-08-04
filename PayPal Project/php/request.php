189fb6a6d320a275c5d3129ee8a311fed490393a
TomMeehan123 <58112746+TomMeehan123@users.noreply.github.com>
2021-08-04 13:38:09 +0100

    Update request.php

--- PayPal Project/php/request.php
+++ PayPal Project/php/request.php
@@ -1,16 +1,77 @@
 <?php
-if ($_POST) // If form was submited...
+
+if ($_POST)
 {
-    $api_endpoint = $_POST["api_endpoint"];
-    $request_body = $_POST["request_body"]; // Get it into a variable
-    print_r( "Api Enpoint: " . $api_endpoint); // Print it!
-    print_r('<br>');
-    print_r("Request Body: " . $request_body);
-    //print_r($_POST);
+    switch($_POST['request_name'])
+    {
+        case "AccessToken":
+            include 'php/access_token.php';
+            break;
 
-}
-else{
-    echo "failure";
-}
+        // Payments
+        case "CreateOrder":
+            include 'php/create_payment.php';
+            break;
+        case "ShowOrder":
+            include 'php/show_order.php';
+            break;
+        case "CaptureOrder":
+            include 'php/capture_payment.php';
+            break;
+        case "AuthoriseOrder":
+            include 'php/authorise_order.php';
+            break;
+        case "RefundOrder":
+            include 'php/refund_order.php';
+            break;
 
+        // Reference Transactions
+        case "CreateToken":
+            include "create_token.php";
+            break;
+        case "ShowToken":
+            include "show_token.php";
+            break;
+        case "CreateAgreement":
+            include "create_agreement.php";
+            break;
+        case "ShowAgreement":
+            include "show_agreement.php";
+            break;
+        case "CancelAgreement":
+            include "cancel_agreement.php";
+            break;
+
+        // Subscriptions
+        case "CreateProduct":
+            include "create_product.php";
+            break;
+        
+        case "CreatePlan":
+            include "create_plan.php";
+            break;
+
+        case "ActivatePlan":
+            include "activate_plan.php";
+            break;
+
+        case "CreateSubscription":
+            include "create_subscription.php";
+            break;
+        
+        case "ActivateSubscription":
+            include "activate_subscription.php";
+            break;
+
+        case "SuspendSubscription":
+            include "suspend_subscription.php";
+            break;
+
+        case "AuthoriseSubscription":
+            include "authorise_subscription.php";
+            break;
+          
+    }
+}
+// $api_endpoint = $response["links"][0]["href"];
 ?>
